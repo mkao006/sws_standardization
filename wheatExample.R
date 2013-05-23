179d0209f72c2db25cf4019540237ab77d045f68
mkao006 <mkao006@gmail.com>
2013-05-23 13:12:29 +0200

    removed the code for merging the tree to toExtractTree.R and added comments to codes

--- wheatExample.R
+++ wheatExample.R
@@ -10,6 +10,9 @@ library(Matrix)
 source("computeDirectExtractRate.R")
 source("read.sua.R")
 
+## Data preperation
+## ---------------------------------------------------------------------
+
 ## Read the wheat tree
 wheatTree.dt = data.table(read.csv(file = "wheatTree.csv", header = TRUE,
   stringsAsFactors = FALSE))
@@ -37,72 +40,34 @@ wheatExtract.dt[, value := as.numeric(value)]
 setnames(wheatExtract.dt, old = "value", new = "ExtractRate")
 setkeyv(wheatExtract.dt, c("Area.Code", "Item.Code", "Year"))
 
+## Pre-standardization
+## ---------------------------------------------------------------------
 
-## Split the data by country and year then merge with the tree
-wheatExtract.lst = split(wheatExtract.dt, wheatExtract.dt[, list(Area.Code, Year)])
-wheatExtractTree.lst = lapply(X = wheatExtract.lst,
-  FUN = function(x) merge(wheatTree.dt, x, all.x = TRUE, allow.cartesian = TRUE,
-    by = "Item.Code"))
-
-## Items which are not in the tree
-setdiff(unique(wheatExtract.dt$Item.Code), wheatTree.dt$Item.Code)
-unique(FAOmetaTable$itemTable[FAOmetaTable$itemTable$itemCode %in%
-                              setdiff(unique(wheatExtract.dt$Item.Code),
-                                      wheatTree.dt$Item.Code), "itemName"])
-
-## Fill the extraction with the defaultif missing
-fillDefaultExtractRate = function(Data, specific, default){
-  setnames(Data, old = c(specific, default), new = c("specific", "default"))
-  Data[is.na(specific), specific := as.numeric(default)]
-  setnames(Data, new = c(specific, default), old = c("specific", "default"))
-}
-
-wheatExtractTree.lst = lapply(X = wheatExtractTree.lst,
-  FUN = fillDefaultExtractRate, specific = "ExtractRate",
-  default = "Default.Extraction.Rate")
-
-
-## Fill the missing country as a result of the join
-fillCountry = function(Data, country){
-  setnames(Data, old = country, new = "country")
-  Data[is.na(country), country := unique(na.omit(Data[, country]))]
-  setnames(Data, new = country, old = "country")
-}
-
-wheatExtractTree.lst = lapply(X = wheatExtractTree.lst,
-  FUN = fillCountry, country = "Area.Code")
-
-
-
-## Fill the missing year as a result of the join
-fillYear = function(Data, year){
-  setnames(Data, old = year, new = "year")
-  Data[is.na(year), year := unique(na.omit(Data[, year]))]
-  setnames(Data, new = year, old = "year")
-}
-
-wheatExtractTree.lst = lapply(X = wheatExtractTree.lst,
-  FUN = fillYear, year = "Year")
-
-
+## split and merge the extraction rate data with the tree
+wheatExtractTree.lst = toExtractTree(extractData = wheatExtract.dt,
+  treeData = wheatTree.dt,
+  splitKey = wheatExtract.dt[, list(Area.Code, Year)],
+  itemKey = "Item.Code", areaCol = "Area.Code", yearCol = "Year",
+  extractSpecificCol = "ExtractRate",
+  extractDefaultCol = "Default.Extraction.Rate")
 
 
 ## Compute the direct extraction rate
 wheatDirectExtractTree.lst = vector(mode = "list",
   length = length(wheatExtractTree.lst))
 for(i in 1:length(wheatExtractTree.lst)){
-  wheatDirectExtractTree.lst[[i]] = merge(computeDirectExtractRate(Data =
-        data.frame(wheatExtractTree.lst[[i]]),
-                                 child = "Item.Code", parent = "Parent.Code",
-                                 ExtractionRate = "ExtractRate"),
-        wheatExtractTree.lst[[i]])
+  wheatDirectExtractTree.lst[[i]] =
+    merge(computeDirectExtractRate(Data = wheatExtractTree.lst[[i]],
+                                   child = "Item.Code", parent = "Parent.Code",
+                                   ExtractionRate = "ExtractRate"),
+          wheatExtractTree.lst[[i]])
 }
 
-## Take the unique extraction rate table
-wheatDirectExtractTree.dt = unique(data.table(subset(do.call("rbind",
+## If the data has multiple paths, then the unique rate needs to be taken. 
+wheatDirectExtractTree.dt = data.table(subset(do.call("rbind",
   wheatDirectExtractTree.lst),
   select = c("Area.Code", "Item.Code", "Item.Name", "Year", "Primary",
-    "Primary.Extraction.Rate", "weight"))))
+    "Primary.Extraction.Rate", "weight")))
   
   
 
@@ -123,20 +88,24 @@ colnames(wheatStandard.df) = c("Area.Code", "Year", "wheatImport", "wheatExport"
 
 
 ## Checks
+## ---------------------------------------------------------------------
+
+## Download the FBS data
 wheatFBS.lst = getFAOtoSYB(name = c("wheatProductProd", "wheatProductImp",
                              "wheatProductExp"), domainCode = rep("FB", 3),
   elementCode = c(5511, 5611, 5911), itemCode = rep(2511, 3))
 wheatFBS.df = subset(wheatFBS.lst$entity, Year %in% wheatStandard.df$Year)
 colnames(wheatFBS.df)[1] = "Area.Code"
 
+## Download the production of the primary commodity
 wheatProd.lst = getFAOtoSYB(name = "wheatProd", domainCode = "QC",
   elementCode = 5510, itemCode = 15)
 wheatProd.df = subset(wheatProd.lst$entity, Year %in% wheatStandard.df$Year)
 colnames(wheatProd.df)[1] = "Area.Code"
 
+## Merge the data and check
 wheatAll.df = merge(wheatStandard.df, wheatProd.df, by = c("Area.Code", "Year"),
   all = TRUE)
-
 
 check = arrange(merge(wheatAll.df, wheatFBS.df, by = c("Area.Code", "Year"),
   all = TRUE), Area.Code, Year)
