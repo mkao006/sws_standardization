c1f915b09f19774d2823575218f977d765a5e1ca
mkao006 <mkao006@gmail.com>
2013-05-23 13:12:57 +0200

    Function to fill in the missing values as a result of cartesian join

---
+++ fillMissingCartesian.R
@@ -0,0 +1,17 @@
+########################################################################
+## Title: A function to fill in missing area, year and extraction rate
+##        as a result of cartesian join.
+## Date: 2013-05-23
+########################################################################
+
+fillMissingCartesian = function(Data, areaCol, yearCol, extractSpecificCol,
+  extractDefaultCol){
+  setnames(Data, old = c(extractSpecificCol, extractDefaultCol, areaCol, yearCol),
+           new = c("specific", "default", "areaCol", "yearCol"))
+  Data[is.na(specific), specific := as.numeric(default)]
+  Data[is.na(areaCol), areaCol := unique(na.omit(Data[, areaCol]))]
+  Data[is.na(yearCol), yearCol := unique(na.omit(Data[, yearCol]))]
+  setnames(Data, new = c(extractSpecificCol, extractDefaultCol, areaCol, yearCol),
+           old = c("specific", "default", "areaCol", "yearCol"))
+  Data
+}
