c8bd517f8cfd0ed606b1012526b523d2fdaff59a
mkao006 <mkao006@gmail.com>
2013-05-24 19:11:37 +0200

    First commit of code to examine the SUA/FBS tree

---
+++ treeExamine.R
@@ -0,0 +1,41 @@
+########################################################################
+## Title: Examine the test tree
+## Date: 2013-05-24
+########################################################################
+
+library(gdata)
+library(igraph)
+igraph.options(label.cex = 0.5)
+
+
+fullTree.dt = data.table(read.csv(file = "tree_test.csv", header = TRUE,
+  stringsAsFactors = FALSE))
+## fullTree.dt = data.table(read.xls(xls = "tree_test.xlsx", header = TRUE,
+##   stringsAsFactors = FALSE))
+setkeyv(fullTree.dt, "Item.Code")
+
+fullTree.graph = graph.data.frame(fullTree.dt[, list(Item.Name, New.Parent.Name)])
+uniqueParent = unique(fullTree.dt[Item.Name == Parent.Name, New.Parent.Name])
+pdf(file = "commodity_trees.pdf")
+for(i in 1:length(uniqueParent)){
+tmp.graph = subgraph(fullTree.graph, V(fullTree.graph)
+  [which(is.finite(shortest.paths(fullTree.graph, to = uniqueParent[i])))]$name)
+if(length(V(tmp.graph)) >= 2)
+  plot(tmp.graph)
+}
+graphics.off()
+system("evince commodity_trees.pdf&")
+
+
+
+FBSTree.graph = graph.data.frame(fullTree.dt[, list(Item.Name, FBS.Parent.Name)])
+uniqueParent = unique(fullTree.dt[FBS.Parent.Code >= 2511, FBS.Parent.Name])
+pdf(file = "fbs_trees.pdf", width = 15, height = 15)
+for(i in 1:length(uniqueParent)){
+tmp.graph = subgraph(FBSTree.graph, V(FBSTree.graph)
+  [which(is.finite(shortest.paths(FBSTree.graph, to = uniqueParent[i])))]$name)
+if(length(V(tmp.graph)) >= 2)
+  plot.igraph(tmp.graph)
+}
+graphics.off()
+system("evince fbs_trees.pdf&")
