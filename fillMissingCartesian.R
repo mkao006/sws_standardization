bb122cb109062010717294bf82aa15ea6d8ad268
mkao006 <mkao006@gmail.com>
2013-06-17 15:28:13 +0200

    Replace zero extraction rate with default extaction rate

--- fillMissingCartesian.R
+++ fillMissingCartesian.R
@@ -9,6 +9,9 @@ fillMissingCartesian = function(Data, areaCol, yearCol, extractSpecificCol,
   setnames(Data, old = c(extractSpecificCol, extractDefaultCol, areaCol, yearCol),
            new = c("specific", "default", "areaCol", "yearCol"))
   Data[is.na(specific), specific := as.numeric(default)]
+  ## NOTE (Michael): This is associated with github issue #17 where
+  ##                 zero extraction rate is causing a problem.
+  Data[specific == 0, specific := as.numeric(default)]  
   Data[is.na(areaCol), areaCol := unique(na.omit(Data[, areaCol]))]
   Data[is.na(yearCol), yearCol := unique(na.omit(Data[, yearCol]))]
   setnames(Data, new = c(extractSpecificCol, extractDefaultCol, areaCol, yearCol),
