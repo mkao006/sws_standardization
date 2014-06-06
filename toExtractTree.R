147272a2102c86aa60ae75b524727a9fc85773bf
mkao006 <mkao006@gmail.com>
2014-06-06 15:32:56 +1200

    Changed indentation and added some brief comments

--- toExtractTree.R
+++ toExtractTree.R
@@ -4,16 +4,21 @@
 ## Date: 2013-05-23
 ########################################################################
 
-toExtractTree = function(extractData, treeData, splitKey, itemKey = "Item.Code",
-  areaCol, yearCol, extractSpecificCol, extractDefaultCol){
-  extract.lst = split(x = extractData, f = splitKey)
+toExtractTree = function(extractData, treeData, splitKey,
+    itemKey = "Item.Code", areaCol, yearCol, extractSpecificCol,
+    extractDefaultCol){
+    extract.lst = split(x = extractData, f = splitKey)
 
-  extractTree.lst = lapply(X = extract.lst,
-    FUN = function(x) merge(treeData, x, all.x = TRUE, allow.cartesian = TRUE,
-    by = itemKey))
+    ## Merge country specific rate data to the default rate and
+    ## item to FBS relationship network.
+    extractTree.lst = lapply(X = extract.lst,
+        FUN = function(x) merge(treeData, x, all.x = TRUE,
+            allow.cartesian = TRUE, by = itemKey))
 
-  extractTreeFull.lst = lapply(X = extractTree.lst, FUN = fillMissingCartesian,
-    areaCol = areaCol, yearCol = yearCol, extractSpecificCol = extractSpecificCol,
-    extractDefaultCol = extractDefaultCol)
-  extractTreeFull.lst
+    ## Fill in missing values as a result of cartesian join.
+    extractTreeFull.lst = lapply(X = extractTree.lst,
+        FUN = fillMissingCartesian, areaCol = areaCol,
+        yearCol = yearCol, extractSpecificCol = extractSpecificCol,
+        extractDefaultCol = extractDefaultCol)
+    extractTreeFull.lst
 }
